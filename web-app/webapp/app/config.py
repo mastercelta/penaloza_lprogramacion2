4c53aa2c516700e56cbf177aea6a3b7faccf108a
Mastercelta <juancamilo@bees.com.co>
2025-03-17 23:18:52 -0500

    Archivos del taller docker

---
+++ web-app/webapp/app/config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL", "postgresql://admin:adminpass@database/productdb"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
